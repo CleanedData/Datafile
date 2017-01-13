a04e3cd8aa11ac02b8a8db027f21ea05ea631bc0
James <jamesrupert2107@gmail.com>
2017-01-13 18:54:10 +0100

    Code revision, serverside.

--- plugin/sv_hooks.lua
+++ plugin/sv_hooks.lua
@@ -1,163 +1,29 @@
 local PLUGIN = PLUGIN;
 
-// FUNCTIONS
+// Create the table for the datafile.
+function PLUGIN:ClockworkDatabaseConnected()
+    local CREATE_DATAFILE_TABLE = [[
+    CREATE TABLE IF NOT EXISTS `]]..Clockwork.config:Get("mysql_datafile_table"):Get()..[[` (
+    `_Key` smallint(11) unsigned NOT NULL AUTO_INCREMENT,
+    `_CharacterID` varchar(50) NOT NULL,
+    `_CharacterName` varchar(150) NOT NULL,
+    `_SteamID` varchar(60) NOT NULL,
+    `_Schema` text NOT NULL,
+    `_Datafile` text NOT NULL,
+    PRIMARY KEY (`_Key`),
+    KEY `_CharacterName` (`_CharacterName`),
+    KEY `_SteamID` (`_SteamID`))
+    ]];
 
--- Called when a player spawns, sets their datafile table up (dfMain).
+    Clockwork.database:Query(string.gsub(CREATE_DATAFILE_TABLE, "%s", " "), nil, nil, true);
+end;
+
+// Check if the player has a datafile or not. If not, create one.
 function PLUGIN:PostPlayerSpawn(player)
-    if (player:GetCharacterData("dfMain") == nil && !table:HasValue(PLUGIN.NoDatafile, player:GetFaction())) then
-        local dfPlayer = {
-            bol = {false, ""},
-            civilStatus = "Citizen",
-            cid = math.random(0, 99999),
-            lastSeen = {"", ""},
-            datafile = {},
-        };
+    local bHasDatafile = PLUGIN:HasDatafile(player);
 
-        player:SetCharacterData("dfMain", dfPlayer);
-
-        /*
-            Darafile design:
-
-            dfPlayer.datafile[k] = {
-                category = "", -- note, civil, Medical
-                text = "",
-                date = "",
-                points = "",
-                originalposter = "",
-            };
-        */
+    // Nil because the bHasDatafile is not in every player their character data.
+    if ((!bHasDatafile || bHasDatafile == nil) && PLUGIN:IsRestrictedFaction(player)) then
+        PLUGIN:CreateDatafile(player);
     end;
 end;
-
--- Adds an entry to a player their datafile.
-function PLUGIN:AddEntry(target, category, text, points, author)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    local count = table.Count(dfPlayer.datafile) + 1;
-
-    dfPlayer.datafile[count] = {
-        category = category,
-        text = text,
-        date = os.date("%d/%m/%Y - %H:%M:%S", os.time()),
-        points = points,
-        originalposter = author:Nick(),
-    };
-
-    target:SetCharacterData("dfMain", dfPlayer);
-end;
-
--- Removes an entry from a player, check if several entries match to ensure it's the correct key.
-function PLUGIN:RemoveEntry(target, key, date, text)
-    local dfPlayer = target:GetCharacterData("dfMain")
-
-    if (dfPlayer.datafile[key].date == date && dfPlayer.datafile[key].text == text) then
-        table.remove(dfPlayer.datafile, key);
-        target:SetCharacterData("dfMain", dfPlayer);
-    end;
-end;
-
--- Updates a player their Civil Status, also adds an entry.
-function PLUGIN:UpdateCivilStatus(target, civilStatus, author)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    dfPlayer.civilStatus = civilStatus;
-
-    target:SetCharacterData("dfMain", dfPlayer);
-
-    PLUGIN:AddEntry(target, "note", author:Nick() .. " has set " .. target:Nick() .. "'s Civil Status to " .. civilStatus .. ".", 0, author)
-end;
-
--- Updates the time a player has been last seen.
-function PLUGIN:UpdateLastSeen(target, player)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    dfPlayer.lastSeen = {
-        player:Nick(),
-        os.date("%d/%m/%Y - %H:%M:%S", os.time()),
-    };
-
-    target:SetCharacterData("dfMain", dfPlayer);
-end;
-
--- Add a BOL, insert a note.
-function PLUGIN:AddBOL(target, text, author)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    dfPlayer.bol = {
-        true,
-        text,
-    };
-
-    target:SetCharacterData("dfMain", dfPlayer);
-
-    PLUGIN:AddEntry(target, "note", auhtor:Nick() .. " has put a BOL on " .. target:Nick() .. ".", 0, author)
-end;
-
--- Remove a BOL, insert a note.
-function PLUGIN:RemoveBOL(target, author)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    dfPlayer.bol = {
-        false,
-        "",
-    };
-
-    target:SetCharacterData("dfMain", dfPlayer);
-
-    PLUGIN:AddEntry(target, "note", author:Nick() .. " has removed a BOL from " .. target:Nick() .. ".", 0, author)
-end;
-
--- Scrub a player their entire datafile.
-function PLUGIN:ScrubDatafile(target)
-    local dfPlayer = {
-        bol = {false, ""},
-        civilStatus = "Citizen",
-        tier = "None",
-        cid = math.random(0, 99999),
-        lastSeen = {"", ""},
-        datafile = {},
-    };
-
-    player:SetCharacterData("dfMain", dfPlayer);
-end;
-
-// DATASTREAM
-
-Clockwork.datastream:Hook("AddEntry", function(player, data)
-    local target = data[1];
-    local category = data[2];
-    local text = data[3];
-    local points = data[4];
-
-    PLUGIN:AddEntry(target, category, text, points, player);
-end)
-
-Clockwork.datastream:Hook("UpdateCivilStatus", function(player, data)
-    local target = data[1];
-    local civilStatus = data[2];
-
-    PLUGIN:UpdateCivilStatus(target, civilStatus, player);
-end);
-
-Clockwork.datastream:Hook("AcknowledgeExistence", function(player, data)
-    local target = data;
-
-    PLUGIN:UpdateLastSeen(target, player)
-end);
-
-Clockwork.datastream:Hook("RemoveEntry", function(player, data)
-    local target = data[1];
-    local key = data[2];
-    local date = data[3];
-    local text = data[4];
-
-    PLUGIN:RemoveEntry(target, key, date, text);
-end);
-
-Clockwork.datastream:Hook("AddBOL", function(player, data)
-    local target = data[1];
-    local text = data[2];
-
-    PLUGIN:AddBOL(target, text, player);
-end);
-
-Clockwork.datastream:Hook("RemoveBOL", function(player, data)
-    local target = data;
-
-    PLUGIN:RemoveBOL(target, player);
-end);
--- plugin/sv_plugin.lua
+++ plugin/sv_plugin.lua
@@ -1,30 +1,34 @@
---[[
-    Initialize a global variable Datafile and put some functions in it.
-    And create some generic PLUGIN functions.
-]]--
-
-Datafile = {};
-
 local PLUGIN = PLUGIN;
 
+Clockwork.config:Add("mysql_datafile_table", "datafile", nil, nil, true, true, true);
+
+PLUGIN.Types = {
+    "med",      // Medical note.
+    "union",    // Union (CWU, WI, UP) type note.
+    "civil",    // Civil Protection/CTA type note.
+};
+
 PLUGIN.Permissions = {
+    elevated = {
+        "Overwatch",
+    },
     full = {
         "Combine Transhuman Arm",
         "Civil Administration Board",
-        "Overwatch",
+        "Civil Protection",
     },
     medium = {
-        "Civil Protection",
         "Server Administration"
     },
     minor = {
         "Civil Worker's Union",
         "Willard Industries",
         "Unity Party",
-    }
-}
+    },
+};
 
-PLUGIN.NoDatafile = {
+// Factions that do not get access to the datafile & factions that don't get a datafile.
+PLUGIN.RestrictedFactions = {
     "Alien Grunt",
     "Bird",
     "Houndeye",
@@ -33,169 +37,42 @@ PLUGIN.NoDatafile = {
     "Houndeye"
 };
 
--- Returns an int, the total points of a player.
-function Datafile:ReturnPoints(target)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    local totalPoints = 0;
+// Create a datafile for the player.
+function PLUGIN:CreateDatafile(player)
+    local schemaFolder = Clockwork.kernel:GetSchemaFolder();
+    local datafileTable = Clockwork.config:Get("mysql_datafile_table"):Get();
+    local character = player:GetCharacter();
+    local steamID = player:SteamID();
 
-    for k, v in pairs(dfPlayer.datafile) do
-        totalPoints = totalPoints + tonumber(v.points);
+    // Set all the values.
+    local queryObj = Clockwork.database:Insert(datafileTable);
+        queryObj:SetValue("_CharacterID", character.characterID);
+        queryObj:SetValue("_CharacterName", character.name);
+        queryObj:SetValue("_SteamID", steamID);
+        queryObj:SetValue("_Schema", schemaFolder);
+        queryObj:SetValue("_Datafile", "this will hopefully be the datafile");
+    queryObj:Push();
+
+    // Change the hasDatafile bool to true to indicate the player has a datafile now.
+    player:SetCharacterData("hasDatafile", true);
+end;
+
+// Returns true if the player has a datafile.
+function PLUGIN:HasDatafile(player)
+    local bHasDatafile = player:GetCharacterData("hasDatafile");
+
+    return bHasDatafile;
+end;
+
+function PLUGIN:UpdateDatafile(player, fileTable)
+
+end;
+
+// If the player is apart of any of the factions within PLUGIN.RestrictedFactions, return true.
+function PLUGIN:IsRestrictedFaction(player)
+    if (table:HasValue(PLUGIN.RestrictedFactions, player:GetFaction())) then
+        return true;
+    else
+        return false;
     end;
-
-    return totalPoints;
-end;
-
---[[ Returns a string, the Civil Status of a player. These can be:
-    Anti-Citizen, Citizen, Black, Brown, Red, Blue, Green, White,
-    Gold, and Platinum.
-]]--
-function Datafile:ReturnCivilStatus(target)
-    local dfPlayer = target:GetCharacterData("dfMain");
-
-    return dfPlayer.civilStatus;
-end;
-
--- Returns a boolean, whether the player has a BOL or not.
-function Datafile:IsBOL(target)
-    local dfPlayer = target:GetCharacterData("dfMain");
-
-    return dfPlayer.bol[1];
-end;
-
--- Returns a boolean, whether the player is an Anti-Citizen or not.
-function Datafile:IsAntiCitizen(target)
-    local dfPlayer = target:GetCharacterData("dfMain");
-
-    return (dfPlayer.civilStatus == "Anti-Citizen");
-end;
-
-function PLUGIN:DecideDatafile(player, target)
-    local playerValue, playerVersion = PLUGIN:ReturnPlayerValue(player);
-    local targetValue, targetVersion = PLUGIN:ReturnPlayerValue(target);
-
-    if (playerValue < targetValue) then
-        Clockwork.player:Notify(player, "You are not allowed to access this datafile.");
-
-    elseif (playerValue > targetValue) then
-        Clockwork.datastream:Start(player, playerVersion + "Datafile", {target, target:GetCharacterData("dfMain")});
-
-    elseif (playerValue == targetValue) then
-        Clockwork.datastream:Start(player, playerVersion + "Datafile", {target, target:GetCharacterData("dfMain")});
-    end;
-end;
-
-function PLUGIN:ReturnPlayerValue(player)
-    local playerFaction = player:GetFaction()
-
-    if (table.HasValue(PLUGIN.Permissions.minor, playerFaction)) then
-        return 1, "minor";
-
-    elseif (table.HasValue(PLUGIN.Permissions.medium, playerFaction)) then
-        return 2, "medium";
-
-    elseif (table.HasValue(PLUGIN.Permissions.full, playerFaction))
-        return 3, "full";
-    end;
-end;
-
--- Send the minor datafile to the target.
-function PLUGIN:SendMinorDatafile(target)
-
-end;
-
--- Send the medium datafile to the target.
-function PLUGIN:SendMediumDatafile(target)
-
-end;
-
--- Send the full datafile to the target.
-function PLUGIN:SendFullDatafile(target)
-
-end;
-
--- Adds an entry to a player their datafile.
-function PLUGIN:AddEntry(target, category, text, points, author)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    local count = table.Count(dfPlayer.datafile) + 1;
-
-    dfPlayer.datafile[count] = {
-        category = category,
-        text = text,
-        date = os.date("%d/%m/%Y - %H:%M:%S", os.time()),
-        points = points,
-        originalposter = author:Nick(),
-    };
-
-    target:SetCharacterData("dfMain", dfPlayer);
-end;
-
--- Removes an entry from a player, check if several entries match to ensure it's the correct key.
-function PLUGIN:RemoveEntry(target, key, date, text)
-    local dfPlayer = target:GetCharacterData("dfMain")
-
-    if (dfPlayer.datafile[key].date == date && dfPlayer.datafile[key].text == text) then
-        table.remove(dfPlayer.datafile, key);
-        target:SetCharacterData("dfMain", dfPlayer);
-    end;
-end;
-
--- Updates a player their Civil Status, also adds an entry.
-function PLUGIN:UpdateCivilStatus(target, civilStatus, author)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    dfPlayer.civilStatus = civilStatus;
-
-    target:SetCharacterData("dfMain", dfPlayer);
-
-    PLUGIN:AddEntry(target, "note", author:Nick() .. " has set " .. target:Nick() .. "'s Civil Status to " .. civilStatus .. ".", 0, author)
-end;
-
--- Updates the time a player has been last seen.
-function PLUGIN:UpdateLastSeen(target, player)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    dfPlayer.lastSeen = {
-        player:Nick(),
-        os.date("%d/%m/%Y - %H:%M:%S", os.time()),
-    };
-
-    target:SetCharacterData("dfMain", dfPlayer);
-end;
-
--- Add a BOL, insert a note.
-function PLUGIN:AddBOL(target, text, author)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    dfPlayer.bol = {
-        true,
-        text,
-    };
-
-    target:SetCharacterData("dfMain", dfPlayer);
-
-    PLUGIN:AddEntry(target, "note", auhtor:Nick() .. " has put a BOL on " .. target:Nick() .. ".", 0, author)
-end;
-
--- Remove a BOL, insert a note.
-function PLUGIN:RemoveBOL(target, author)
-    local dfPlayer = target:GetCharacterData("dfMain");
-    dfPlayer.bol = {
-        false,
-        "",
-    };
-
-    target:SetCharacterData("dfMain", dfPlayer);
-
-    PLUGIN:AddEntry(target, "note", author:Nick() .. " has removed a BOL from " .. target:Nick() .. ".", 0, author)
-end;
-
--- Scrub a player their entire datafile.
-function PLUGIN:ScrubDatafile(target)
-    local dfPlayer = {
-        bol = {false, ""},
-        civilStatus = "Citizen",
-        tier = "None",
-        cid = math.random(0, 99999),
-        lastSeen = {"", ""},
-        datafile = {},
-    };
-
-    player:SetCharacterData("dfMain", dfPlayer);
 end;
