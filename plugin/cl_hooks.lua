78ee30f7accabfc259a2cf3183ae5d19930bca57
Alexander Grist-Hucker <gristhavoc@gmail.com>
2017-04-19 23:30:54 +0100

    Fixed an incorrect datastream name.

--- plugin/cl_hooks.lua
+++ plugin/cl_hooks.lua
@@ -16,17 +16,17 @@ Clockwork.datastream:Hook("CreateFullDatafile", function(data)
 	local target = data[1];
 	local GenericData = data[2]
 	local datafile = data[3];
-	
+
 	PLUGIN.Datafile = vgui.Create("cwFullDatafile");
 	PLUGIN.Datafile:PopulateDatafile(target, datafile);
 	PLUGIN.Datafile:PopulateGenericData(target, datafile, GenericData);
 end);
 
 -- Management panel, for removing entries.
-Clockwork.datastream:Hook("CreateFullDatafile", function(data)
+Clockwork.datastream:Hook("CreateManagementPanel", function(data)
 	local target = data[1];
 	local datafile = data[2];
-	
+
 	PLUGIN.Managefile = vgui.Create("cwDfManageFile");
 	PLUGIN.Managefile:PopulateEntries(target, datafile);
 end);
